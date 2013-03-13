5ebc3f2c47ade627d31d496668332de28759733e
babofitos <tehchoi@gmail.com>
2013-03-13 18:05:19 -0400

    initial commit

---
+++ routes/session.js
@@ -0,0 +1,26 @@
+/*
+* Session Routes
+*/
+var users = require('../data/users')
+  , notLoggedIn = require('./middleware/not_logged_in')
+
+module.exports = function(app) {
+  app.get('/session/new', notLoggedIn, function(req, res) {
+    res.render('session/new', {title: "Log in"});
+  });
+  
+  app.post('/session', notLoggedIn, function(req, res) {
+    if (users[req.body.username] &&
+      users[req.body.username].password === req.body.password) {
+        req.session.user = users[req.body.username];
+        res.redirect('/users');
+    } else {
+      res.redirect('/session/new')
+    }
+  });
+
+  app.del('/session', function(req, res, next) {
+    req.session.destroy();
+    res.redirect('/users');
+  });
+};
