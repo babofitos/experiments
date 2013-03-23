eccf80d8104d213f937435c42e3250c9ce4dd6e9
babofitos <tehchoi@gmail.com>
2013-03-22 20:24:26 -0400

    add mosaic

--- canvas/mosaic/script.js
+++ canvas/mosaic/script.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById('canvas')
   , imageData = context.createImageData(400, 400)
   , pixels = imageData.data
 
-function static () {
+function static (context, pixels, imageData) {
   for (var i=0;i<pixels.length;i++) {
     pixels[i*4] = Math.floor(Math.random()*255) //r
     pixels[i*4+1] = Math.floor(Math.random()*255) //g
@@ -13,4 +13,37 @@ function static () {
   context.putImageData(imageData, 0, 0)
 }
 
-window.setInterval(static, 200)
+// window.setInterval(function(context, pixels, imageData) {
+//   static(context,pixels,imageData)
+// }, 200)
+
+function mosaic (context, pixels, imageData, row, col) {
+  var tileHeight = imageData.height/col
+    , tileWidth = imageData.width/row
+    , totalTiles = col*row
+    , tileArea = tileWidth*tileHeight
+
+  for (var r=0;r<row;r++) { //row
+    for (var c=0;c<col;c++) { //column
+      var red = Math.floor(Math.random()*255)
+        , green = Math.floor(Math.random()*255)
+        , blue = Math.floor(Math.random()*255)
+
+      for (var tiler=0;tiler<tileHeight;tiler++) { //tile row
+        for (var tilec=0;tilec<tileWidth;tilec++) { //tile col
+          var trueX = (c*tileWidth)+tilec
+            , trueY = (r*tileHeight)+tiler
+            , pos = (trueY*(imageData.width*4))+(trueX*4)
+
+          pixels[pos] = red
+          pixels[pos+1] = green
+          pixels[pos+2] = blue
+          pixels[pos+3] = 255
+        }
+      }
+    }
+  }
+  context.putImageData(imageData, 0, 0)
+}
+
+mosaic(context, pixels, imageData, 4, 4)
