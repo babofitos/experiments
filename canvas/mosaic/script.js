4d1e040714dcc7d92ad023ce8b8dd31a15c1f0d9
babofitos <tehchoi@gmail.com>
2013-03-22 21:46:56 -0400

    add comments

--- canvas/mosaic/script.js
+++ canvas/mosaic/script.js
@@ -31,8 +31,15 @@ function mosaic (context, pixels, imageData, row, col) {
 
       for (var tiler=0;tiler<tileHeight;tiler++) { //tile row
         for (var tilec=0;tilec<tileWidth;tilec++) { //tile col
+          //to get exact x,y for pixel
           var trueX = (c*tileWidth)+tilec
             , trueY = (r*tileHeight)+tiler
+            //once you have (x,y) you need to convert to pixel array indices
+            //this is already 0 based so no need to -1 from y and x
+            //width*4 will give you the size of one pixelgrid row
+            //multiply that by y to get the first index in a pixelgrid row
+            //x*4 will give you the # of cols before the column you want
+            //add together to get the pixel index that you want
             , pos = (trueY*(imageData.width*4))+(trueX*4)
 
           pixels[pos] = red
