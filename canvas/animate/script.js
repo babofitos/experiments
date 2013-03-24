944b14b5d079a66c1d99fe615c7d11c52ad5fc85
babofitos <tehchoi@gmail.com>
2013-03-23 21:59:15 -0400

    init random shapes

--- canvas/animate/script.js
+++ canvas/animate/script.js
@@ -2,13 +2,19 @@ var canvas = document.getElementById('canvas')
   , ctx = canvas.getContext('2d')
   , shapes = []
 
-shapes.push(new Shape(50, 50))
-shapes.push(new Shape(100, 100))
-shapes.push(new Shape(150, 150))
+for (var i = 0; i < 10; i++) {
+  var x = Math.random()*250
+    , y = Math.random()*250
+    , width = height = Math.random()*50
 
-function Shape(x, y) {
+  shapes.push(new Shape(x, y, width, height))
+}
+
+function Shape(x, y, width, height) {
   this.x = x
   this.y = y
+  this.width = width
+  this.height = height
 }
 
 function animate(ctx, canvas) {
@@ -16,7 +22,7 @@ function animate(ctx, canvas) {
   for (var i = 0; i < shapes.length; i++) {
     var tmpshape = shapes[i]
     tmpshape.x++
-    ctx.fillRect(tmpshape.x, tmpshape.y, 10, 10)
+    ctx.fillRect(tmpshape.x, tmpshape.y, tmpshape.width, tmpshape.height)
   }
   setTimeout(function() {
     animate(ctx, canvas)
