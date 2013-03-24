a3a2a1bb6a323434aaf98222cf4a788c24bc714d
babofitos <tehchoi@gmail.com>
2013-03-23 21:42:04 -0400

    objects and arrays

--- canvas/animate/script.js
+++ canvas/animate/script.js
@@ -1,11 +1,23 @@
-var x = 0
-  , canvas = document.getElementById('canvas')
+var canvas = document.getElementById('canvas')
   , ctx = canvas.getContext('2d')
+  , shapes = []
+
+shapes.push(new Shape(50, 50))
+shapes.push(new Shape(100, 100))
+shapes.push(new Shape(150, 150))
+
+function Shape(x, y) {
+  this.x = x
+  this.y = y
+}
 
 function animate(ctx, canvas) {
-  x++
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.fillRect(x, 250, 10, 10)
+  for (var i = 0; i < shapes.length; i++) {
+    var tmpshape = shapes[i]
+    tmpshape.x++
+    ctx.fillRect(tmpshape.x, tmpshape.y, 10, 10)
+  }
   setTimeout(function() {
     animate(ctx, canvas)
   }, 33)
