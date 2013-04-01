85b581f85ff634fe7f585bbd2678fad60fc4a5b7
babofitos <tehchoi@gmail.com>
2013-04-01 01:14:14 -0400

    add comments for future reference

--- canvas/circles/script.js
+++ canvas/circles/script.js
@@ -12,6 +12,7 @@ for (var i = 0; i < 10; i++) {
   shapes.push(new Shape(x, y, width, height))
 }
 
+//a rect. the radius and angle is for its rotation, not the shape of the shape
 function Shape(x, y, width, height) {
   this.x = x
   this.y = y
@@ -25,7 +26,17 @@ function animate(ctx, canvas) {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (var i = 0; i < shapes.length; i++) {
     var tmpshape = shapes[i]
+    //since the coords of the center of the orbit do not start at (0,0), 
+    //we must add the center to the adjacent or opposite side for x and y
+    //respectively
+    
+    //convert the angle into radians and get the cos, 
+    //then multiply it by the hypotenuse
+    //(radius) to get the adjacent side (x)
       , x = tmpshape.x + tmpshape.radius*(Math.cos(tmpshape.angle*(Math.PI/180)))
+    //convert the angle into radians and get the sin, 
+    //then multiply it by the hypotenuse
+    //(radius) to get the opposite side (y)
       , y = tmpshape.y + tmpshape.radius*(Math.sin(tmpshape.angle*(Math.PI/180)))
 
     //increase angle for rotation
